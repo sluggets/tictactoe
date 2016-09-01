5cb91813a492015f2dc87c57c189d5b14e839eac
timothy <tim.longoria@gmail.com>
2016-09-01 11:10:59 -0500

    Adds responsive tic-tac-toe, with canvas

--- js/ttt.js
+++ js/ttt.js
@@ -1,1 +1,31 @@
-var begin = "Okay";
+document.addEventListener("DOMContentLoaded", function() {
+  testX();
+});
+
+function testX()
+{
+  XCtr = 285;
+  YCtr = 10;
+  var canvas = document.getElementById("tl");
+  var ctx = canvas.getContext('2d');
+  ctx.beginPath();
+  ctx.moveTo(285, 10);
+  ctx.lineTo(10, 135);
+  ctx.stroke();
+  //intervalID = window.setInterval(animateX, 1, ctx); 
+}
+
+function animateX(context)
+{
+  console.log("xCoor: " + XCtr);
+  console.log("yCoor: " + YCtr);
+  if (XCtr < 10 || YCtr > 135)
+  {
+    clearInterval(intervalID);
+    return;
+  }
+  XCtr--;
+  YCtr++;
+  context.lineTo(XCtr, YCtr);
+  context.stroke();
+}
